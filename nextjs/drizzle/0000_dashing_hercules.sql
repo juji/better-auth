c56665278bc41ce1ac387004dfa69bd9b17829ee
juji <jujiyangasli@gmail.com>
2025-09-25 05:24:05 +0700

    enable row level security

--- nextjs/drizzle/0000_dashing_hercules.sql
+++ nextjs/drizzle/0000_dashing_hercules.sql
@@ -48,3 +48,8 @@ CREATE TABLE "verifications" (
 --> statement-breakpoint
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+
+alter table "accounts" enable row level security;
+alter table "sessions" enable row level security;
+alter table "users" enable row level security;
+alter table "verifications" enable row level security;
