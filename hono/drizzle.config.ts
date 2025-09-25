6a402a774bc304713ae1495bbec2831e28b5c5cd
juji <jujiyangasli@gmail.com>
2025-09-25 22:15:18 +0700

    use uuid for ids

---
+++ hono/drizzle.config.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './lib/drizzle',
+  schema: './lib/db/schema',
+  dialect: 'postgresql',
+  dbCredentials: {
+    ... process.env.POSTGRES_URL ? {
+      url: process.env.POSTGRES_URL!  
+    } : {
+      host: process.env.POSTGRES_HOST!,
+      port: parseInt(process.env.POSTGRES_PORT!),
+      user: process.env.POSTGRES_USER!,
+      password: process.env.POSTGRES_PASSWORD!,
+      database: process.env.POSTGRES_DB!,
+      ...process.env.POSTGRES_CERT ? {
+      ssl: {
+        ca: process.env.POSTGRES_CERT
+      }} : {},
+    }
+  },
+});
--- nextjs/drizzle/0000_flowery_wild_pack.sql
+++ nextjs/drizzle/0000_flowery_wild_pack.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "accounts" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"account_id" text NOT NULL,
 	"provider_id" text NOT NULL,
-	"user_id" text NOT NULL,
+	"user_id" uuid NOT NULL,
 	"access_token" text,
 	"refresh_token" text,
 	"id_token" text,
@@ -15,19 +15,19 @@ CREATE TABLE "accounts" (
 );
 --> statement-breakpoint
 CREATE TABLE "sessions" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"expires_at" timestamp NOT NULL,
 	"token" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp NOT NULL,
 	"ip_address" text,
 	"user_agent" text,
-	"user_id" text NOT NULL,
+	"user_id" uuid NOT NULL,
 	CONSTRAINT "sessions_token_unique" UNIQUE("token")
 );
 --> statement-breakpoint
 CREATE TABLE "users" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
 	"email" text NOT NULL,
 	"email_verified" boolean DEFAULT false NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE "users" (
 );
 --> statement-breakpoint
 CREATE TABLE "verifications" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"identifier" text NOT NULL,
 	"value" text NOT NULL,
 	"expires_at" timestamp NOT NULL,
