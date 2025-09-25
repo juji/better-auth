bd12f34964267c656856b3ff17e3a484e00a66ea
juji <jujiyangasli@gmail.com>
2025-09-25 08:35:11 +0700

    rejectUnauthozrized: false for aiven

--- nextjs/db/index.ts
+++ nextjs/db/index.ts
@@ -1,4 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-export const db = drizzle(
-  process.env.POSTGRES_URL!
-);
+import { Pool } from 'pg';
+
+// since we are using aiven with self-signed certificates
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false, // This allows self-signed certificates
+  },
+});
+
+export const db = drizzle(pool);
