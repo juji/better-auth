02502dbc404047507a4486ddead01fcc131464ee
juji <jujiyangasli@gmail.com>
2025-09-25 08:38:02 +0700

    rejectUnauthozrized: false for aiven

--- nextjs/drizzle.config.ts
+++ nextjs/drizzle.config.ts
@@ -1,19 +1,12 @@
 // import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-console.log('Database URL:', process.env.POSTGRES_URL);
-console.log('Database URL string length:', process.env.POSTGRES_URL?.length);
-
 export default defineConfig({
   out: './drizzle',
   schema: './db/schema',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
+    ssl: true, // Enable SSL with rejectUnauthorized: false by default
   },
-  ssl: {
-    rejectUnauthorized: false, // This allows self-signed certificates
-  },
-
-  
 });
