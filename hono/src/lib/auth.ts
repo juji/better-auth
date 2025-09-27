1ed909cb7c9e1ab961750a44023331b54a5a6722
juji <jujiyangasli@gmail.com>
2025-09-27 08:25:50 +0700

    google login: add select_account

--- hono/src/lib/auth.ts
+++ hono/src/lib/auth.ts
@@ -90,7 +90,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
     google: { 
-      
+      prompt: "select_account", 
       clientId: process.env.GOOGLE_CLIENT_ID as string, 
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
