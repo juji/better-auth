8418edb25b7abc422b01219663d0315e23be7c65
juji <jujiyangasli@gmail.com>
2025-09-29 06:38:20 +0700

    Update express auth-client to match nextjs implementation

--- express/src/lib/auth-client.ts
+++ express/src/lib/auth-client.ts
@@ -1,16 +1,12 @@
 import { createAuthClient } from "better-auth/client"
 import { bearer } from "better-auth/plugins";
 
-export const authClient = createAuthClient({
-  baseURL: (process.env.AUTH_SERVER || "http://localhost:3001") + '/auth',
-  plugins: [
-    bearer() 
-  ]
-})
-
 export async function getUserSession({ token }: { token: string }) {
   const authClient = createAuthClient({
     baseURL: (process.env.AUTH_SERVER || "http://localhost:3001") + '/auth',
+    plugins: [
+      bearer() 
+    ],
     fetchOptions: {
       auth: {
         type:"Bearer",
