f42a33e93279e6c0a97426dcee3348d71b7361f0
juji <jujiyangasli@gmail.com>
2025-09-29 04:05:33 +0700

    cleanup

--- nextjs/app/api/protected/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { checkAuth } from "@/lib/check-auth";
-
-export async function GET(){
-
-  return await checkAuth(( session ) => Response.json({ message: 42, authSession: session }) );
-
-}
