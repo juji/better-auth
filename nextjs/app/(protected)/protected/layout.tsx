95b0e5f935fe7bf87e622ef1bd11ebdeee0ca5fb
juji <jujiyangasli@gmail.com>
2025-09-28 09:03:53 +0700

    Make sidebar sticky to top on desktop and improve mobile responsiveness

--- nextjs/app/(protected)/protected/layout.tsx
+++ nextjs/app/(protected)/protected/layout.tsx
@@ -30,7 +30,7 @@ export default function ProtectedPageLayout({
       )}
 
       {/* Sidebar */}
-      <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-black/20 backdrop-blur-xl border-r border-white/10 flex flex-col transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0 ${
+      <div className={`fixed lg:sticky lg:top-0 inset-y-0 left-0 z-50 w-64 h-screen bg-black/20 backdrop-blur-xl border-r border-white/10 flex flex-col transform transition-transform duration-300 ease-in-out lg:translate-x-0 ${
         sidebarOpen ? 'translate-x-0' : '-translate-x-full'
       }`}>
         <div className="p-6">
